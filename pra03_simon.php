66578ee516ab597c52b627acd720d2ed6858476e
simon.lee <chen2020713@gmail.com>
2024-10-14 12:56:05 +0800

    用直角三角形做99乘法表

--- pra03_simon.php
+++ pra03_simon.php
@@ -14,7 +14,7 @@
 <body>
 <?php
     echo "直角三角形<br>";
-    $n=9;
+    $n=9; //邊長
     for($i=1;$i<=$n;$i++)
     {
         for($j=0;$j<$i;$j++)
@@ -133,7 +133,16 @@
 
     }
     echo "菱形對角線<br>";
-    if($n&0x01==1)
+    $n=10;
+    /*
+    //單邊常不需考慮偶數
+    if(($n&0x01)==0)
+    {
+        $n++;
+        echo "偶數加1<br>";
+
+    }
+    */ 
     {
         $k=2*$n;
         for($i=1;$i<$k;$i++)
@@ -158,7 +167,32 @@
     
 
 
-
+    echo "菱形對角線(老師作法)<br>";
+    $size=11;
+        for($i=0;$i<$size;$i++){
+            if($i>(floor($size/2))){
+                $k1=$i-(floor($size/2));  //前半開始點
+                $j1=2*($i-(2*($i-(floor($size/2)))))+1; //數量
+            }else{
+                $k1=(floor($size/2))-$i;   //後半開始點
+                $j1=(2*$i+1);               //數量
+            }
+            
+            for($k=0;$k<$k1;$k++){
+                echo "&nbsp;";
+            }
+        
+            for($j=0;$j<$j1;$j++){
+                if($j==0 || $j==$j1-1 || $i==(floor($size/2)) || $j==floor(($j1-1)/2) ){
+                    echo "*";
+                }else{
+                    echo "&nbsp;";
+                }
+        
+            }
+            echo "<br>";
+        
+        }
 
     echo "矩形<br>";           
     for($i=0;$i<$n;$i++)
@@ -203,7 +237,42 @@
            
         }
         echo "<br>";
-    }     
+    }  
+    
+    echo "用直角三角形做99乘法表<br>";
+    $n=9; //邊長
+    for($i=0;$i<=$n;$i++)
+    {
+        if($i==0 ) 
+        {
+            for($j=0;$j<=$n;$j++) 
+            {
+                $k=$j;   
+            if($k<10)
+                echo "&nbsp&nbsp".$k;
+            else echo "&nbsp".$k;
+            }
+
+        }
+        else 
+        {        
+            for($j=1;$j<=$i+1;$j++)
+            {   
+                if($j>1)
+                    $k=$i*($j-1);
+                else
+                    $k=$i;
+                if($k<10)
+                    echo "&nbsp&nbsp".$k;
+                else
+                    echo "&nbsp".$k;
+            }
+        }
+             
+
+        echo "<br>";
+
+    }    
     echo "新增GIT合併遠端再做merge<br>";  //GIT study    
 ?>
 </body>
